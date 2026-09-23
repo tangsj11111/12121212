1962b4084a1d0b9a448e91ca9759fb98d6175cfd
tanggsj11111 <3256359866@qq.com>
2026-09-23 15:23:06 +0800

    修改1

--- image.py
+++ image.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 def flip_image_horizontally(image_path):                                                                            
 	# 打开原始图片                                                                                                  
 	img = Image.open(image_path)                                                                                    
-	# 左右翻转                                                                                                       
-	flipped_img =                                                                                                   
+	# 左右翻转图片                                                                                                       
+	flipped_img  =                                                                                                   
 img.transpose(Image.FLIP_LEFT_RIGHT)                                                                                
 	# 并排展示原图、翻转图                                                                                           
 	plt.figure(figsize=(10,5))                                                                                      
