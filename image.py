b38b5ba2b96f1dfb2b53a43d3251011e0f1d9ef8
tanggsj11111 <3256359866@qq.com>
2026-09-23 15:27:28 +0800

    修改22行“ git push git commit -m修改22行“

--- image.py
+++ image.py
@@ -4,8 +4,7 @@ def flip_image_horizontally(image_path):
 	# 打开原始图片                                                                                                  
 	img = Image.open(image_path)                                                                                    
 	# 左右翻转图片                                                                                                       
-	flipped_img  =                                                                                                   
-img.transpose(Image.FLIP_LEFT_RIGHT)                                                                                
+	flipped_img  = img.transpose(Image.FLIP_LEFT_RIGHT)                                                                                
 	# 并排展示原图、翻转图                                                                                           
 	plt.figure(figsize=(10,5))                                                                                      
 	plt.subplot(1,2,1)                                                                                              
